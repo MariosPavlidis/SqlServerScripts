925b5e8b23ce902418340d58db4fcb6ce62dfded
MariosPavlidis <pavlidis.marios@gmail.com>
2025-09-02 14:38:56 +0300

    Update BlockingQueries.sql

--- Synapse/BlockingQueries.sql
+++ Synapse/BlockingQueries.sql
@@ -1,9 +1,15 @@
-
+ 
+--blocking
 SELECT
-    TOP 500 blocking.request_id AS BlockingRequestId,waiting.request_id AS WaitingRequestId,
-    blocking.session_id AS BlockingSessionId,   waiting.session_id waitingSession, 
+    TOP 500 blocking.request_id AS BlockingRequestId,
+    waiting.request_id AS WaitingRequestId,
+    blocking.session_id AS BlockingSessionId,   
+    waiting.session_id waitingSession, 
     waiting.type +' '+' Lock On '+waiting.object_type +' ' +waiting.object_name  as WaitResource,
-    waiting.request_time AS ObjectLockRequestTime,datediff (s,waiting.request_time,getdate()) secs_wait ,
+    waiting.request_time AS ObjectLockRequestTime,
+    RIGHT('0' + CAST(DATEDIFF(s,waiting.request_time, GETDATE()) / 3600 AS VARCHAR(2)), 2) + ':' +
+    RIGHT('0' + CAST((DATEDIFF(s,waiting.request_time, GETDATE()) % 3600) / 60 AS VARCHAR(2)), 2) + ':' +
+    RIGHT('0' + CAST(DATEDIFF(s,waiting.request_time, GETDATE()) % 60 AS VARCHAR(2)), 2) AS [hh:mm:ss],
     s.login_name as BlockingLoginName,r.command as BlockingCommand,
     rw.command as WaitingCommand
 FROM
@@ -17,4 +23,7 @@ WHERE
     waiting.state = 'Queued'
     AND blocking.state = 'Granted'
 ORDER BY
-    secs_wait desc;
+        RIGHT('0' + CAST(DATEDIFF(s,waiting.request_time, GETDATE()) / 3600 AS VARCHAR(2)), 2) + ':' +
+    RIGHT('0' + CAST((DATEDIFF(s,waiting.request_time, GETDATE()) % 3600) / 60 AS VARCHAR(2)), 2) + ':' +
+    RIGHT('0' + CAST(DATEDIFF(s,waiting.request_time, GETDATE()) % 60 AS VARCHAR(2)), 2)  desc;
+ 
